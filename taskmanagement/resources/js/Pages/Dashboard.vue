6daa7145b538c856212231589e608539d5670d1d
thomasbockhorn <historian.thomas@gmail.com>
2023-06-13 18:59:59 -0500

    Added demo tasks and styling

--- taskmanagement/resources/js/Pages/Dashboard.vue
+++ taskmanagement/resources/js/Pages/Dashboard.vue
@@ -8,9 +8,13 @@
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 workbench">
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5"
+                    v-for="task in tasks"
+                    :key="task.id"
+                >
+                    <div class="p-6 text-gray-900">{{ task.task_name }}</div>
                 </div>
             </div>
         </div>
@@ -26,5 +30,35 @@ export default {
         AuthenticatedLayout,
         Head,
     },
+    data() {
+        return {
+            tasks: [
+                {
+                    id: 0,
+                    task_name: "Task 1",
+                    priority: 1,
+                },
+                {
+                    id: 1,
+                    task_name: "Task 2",
+                    priority: 2,
+                },
+                {
+                    id: 2,
+                    task_name: "Task 3",
+                    priority: 3,
+                },
+            ],
+        };
+    },
+    methods: {},
 };
 </script>
+
+<style scoped>
+.workbench {
+    background-color: #eee;
+    margin-bottom: 10px;
+    padding: 10px;
+}
+</style>
