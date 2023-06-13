deca16a5216e2da4aefd07d6792ec549bdff3228
thomasbockhorn <historian.thomas@gmail.com>
2023-06-13 18:41:40 -0500

    Finished getting rid of default logo

--- taskmanagement/resources/js/Pages/Dashboard.vue
+++ taskmanagement/resources/js/Pages/Dashboard.vue
@@ -1,14 +1,10 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-</script>
-
 <template>
-    <Head title="Dashboard" />
-
     <AuthenticatedLayout>
+        <Head title="Dashboard" />
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
         </template>
 
         <div class="py-12">
@@ -20,3 +16,15 @@ import { Head } from '@inertiajs/vue3';
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        AuthenticatedLayout,
+        Head,
+    },
+};
+</script>
