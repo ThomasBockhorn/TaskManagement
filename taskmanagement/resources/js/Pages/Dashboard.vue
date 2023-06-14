e3051d86887dcd5ff44b706ea402b6fc51554f85
thomasbockhorn <historian.thomas@gmail.com>
2023-06-13 20:40:23 -0500

    tasks can be dragged

--- taskmanagement/resources/js/Pages/Dashboard.vue
+++ taskmanagement/resources/js/Pages/Dashboard.vue
@@ -8,13 +8,40 @@
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 workbench">
+            <div
+                class="max-w-7xl mx-auto sm:px-6 lg:px-8 workbench"
+                @drop="dropTask($event, 1)"
+                @dragover.prevent
+                @dragenter.prevent
+            >
                 <div
                     class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5"
-                    v-for="task in tasks"
+                    v-for="task in tasksOne"
                     :key="task.id"
+                    draggable="true"
+                    @dragstart="dragTask($event, task)"
                 >
-                    <div class="p-6 text-gray-900">{{ task.task_name }}</div>
+                    <div class="p-6 text-gray-900">
+                        {{ task.task_name }}
+                    </div>
+                </div>
+            </div>
+            <div
+                class="max-w-7xl mx-auto sm:px-6 lg:px-8 workbench"
+                @drop="dropTask($event, 2)"
+                @dragover.prevent
+                @dragenter.prevent
+            >
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5"
+                    v-for="task in tasksTwo"
+                    :key="task.id"
+                    draggable="true"
+                    @dragstart="dragTask($event, task)"
+                >
+                    <div class="p-6 text-gray-900">
+                        {{ task.task_name }}
+                    </div>
                 </div>
             </div>
         </div>
@@ -46,12 +73,31 @@ export default {
                 {
                     id: 2,
                     task_name: "Task 3",
-                    priority: 3,
+                    priority: 1,
                 },
             ],
         };
     },
-    methods: {},
+    computed: {
+        tasksOne() {
+            return this.tasks.filter((task) => task.priority === 1);
+        },
+        tasksTwo() {
+            return this.tasks.filter((task) => task.priority === 2);
+        },
+    },
+    methods: {
+        dragTask(event, task) {
+            event.dataTransfer.dropEffect = "move";
+            event.dataTransfer.effectAllowed = "move";
+            event.dataTransfer.setData("taskID", task.priority);
+        },
+        dropTask(event, priority) {
+            const taskID = event.dataTransfer.getData("taskID");
+            const task = this.tasks.find((task) => task.priority == taskID);
+            task.priority = priority;
+        },
+    },
 };
 </script>
 
