fc22cd7f0fd1f215dd3e78504857664ac508dfc7
thomasbockhorn <historian.thomas@gmail.com>
2023-06-13 21:07:41 -0500

    Added a User Id to tasks

--- taskmanagement/database/migrations/2023_06_13_224343_create_tasks_table.php
+++ taskmanagement/database/migrations/2023_06_13_224343_create_tasks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('task_name');
             $table->integer('priority')->unsigned();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
