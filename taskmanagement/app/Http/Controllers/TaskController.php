9b5daf2739d805511fbbe72387644e64d2abf146
thomasbockhorn <historian.thomas@gmail.com>
2023-06-13 21:03:01 -0500

    TaskController only stores and destroys

---
+++ taskmanagement/app/Http/Controllers/TaskController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
